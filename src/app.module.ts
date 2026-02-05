7435356b52bb645fc7d40d2bae688a3ef662035b
Leonardo Roberto Garcia <cololeo@gmail.com>
2026-02-05 15:44:40 +0100

    Orchestrating controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HealthModule } from './health.module';
+import { OrdersModule } from './orders.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HealthModule, OrdersModule],
 })
 export class AppModule {}
